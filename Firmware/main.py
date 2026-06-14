28c77f3ccb45f442e0fa21c33ec9d298bc486f63
Matty-0 <matthewpoliver16@gmail.com>
2026-06-14 13:40:20 -0400

    Update main.py

--- Firmware/main.py
+++ Firmware/main.py
@@ -1,19 +1,21 @@
+#imports for creating and running the keyboard command
 import board
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
-from kmk.scanners import DiodeOrientation
 
+
+#creates the actual keyboard
 keyboard = KMKKeyboard()
 
-# Define your matrix pins (adjust to match your wiring)
+#Defining what the matrix pins will do for each key
 keyboard.col_pins = (board.D0, board.D1, board.D2)
 keyboard.row_pins = (board.D3,)
-keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
-# Define what each key does
+#Keys are defined here telling them what to do
 keyboard.keymap = [
-    [KC.A, KC.B, KC.C],
+    [KC.W, KC.A, KC.D],
 ]
 
+#statement to run the command "keyboard" if the name is correct
 if __name__ == "__main__":
     keyboard.go()
